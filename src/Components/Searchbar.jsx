a727f0cc9b210a4dccfe9ccf8a26c3d48652ae9f
janette <janet.muange@student.moringa.com>
2026-05-14 17:37:25 +0300

    updated moviePlus app

---
+++ src/Components/Searchbar.jsx
@@ -0,0 +1,12 @@
+function Searchbar({ query, setQuery}) {
+    return (
+        <div className="searchbar">
+            <input 
+            type="text"
+            placeholder="Search movies..."
+            value={query}
+            onChange={(e)=>setQuery(e.target.value)}
+         />
+        </div>
+    );
+}
