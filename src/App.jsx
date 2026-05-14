423cd938327f18ceb10ab95b256b6f2c20d11804
ruriga002 <eruriga002@gmail.com>
2026-05-15 00:16:49 +0300

    ADDED COMMENTS TO APP.JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
+// import React hooks
 import { useState, useEffect } from "react";
+// imprting routing tools from react-router-dom
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import application components
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Moviedetails from "./Components/Moviedetails";
 import Favourites from "./Components/Favourites";
 import Login from "./Components/Login";
+// import application styles
 import "./App.css";
 
 function App() {
+  // store the currently logged-in user
   const [currentUser, setCurrentUser] = useState(null);
 
+  // run once when the app loads
   useEffect(() => {
+    //Get saved user from local storage and set it as the current user
     const saved = localStorage.getItem("user");
     if (saved) setCurrentUser(JSON.parse(saved));
   }, []);
-
+  // function to handle user logout
   const handleLogout = () => {
+    // clear user state
     setCurrentUser(null);
+    // remove user from local storage
     localStorage.removeItem("user");
   };
 
